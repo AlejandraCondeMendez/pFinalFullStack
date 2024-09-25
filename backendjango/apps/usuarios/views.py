52ff1e1bf8e245dd98998542e56c6e0c2db34856
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-09-25 02:30:50 -0600

    La página ya cuenta con el restframework del formlario de registro. También, se hicieron las validaciones de usuario donde si el usuario existe arroja un error y si el usuario no existe, lo crea.

--- backendjango/apps/usuarios/views.py
+++ backendjango/apps/usuarios/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from apps.usuarios.models import Registro
 
 # Create your views here.
 
 class RegistroView(APIView):
+    def post(self, request):
+        usuario = request.data.get('username')
+        contrasena = request.data.get('password')
+        correo = request.data.get('email')
+        tel = request.data.get('telefono')
+        ubi = request.data.get('ubicacion')
     
+    # si ambos usuarios son iguales, ya existe, por ende no va a dejar crear un nuevo usuario
+        if User.objects.filter(username = usuario).exists():
+            return Response({'error': 'Usuario ya existe'}, status=status.HTTP_400_BAD_REQUEST)
+        else: 
+            nuevo_usuario = User.objects.create_user(username=usuario, password=contrasena, email=correo)
+            
+            Registro.objects.create(
+                user = nuevo_usuario,
+                telefono = tel,
+                ubicacion = ubi
+            )
+            
+            return Response({'success': 'Usuario creado'}, status=status.HTTP_201_CREATED)
+               
+        
