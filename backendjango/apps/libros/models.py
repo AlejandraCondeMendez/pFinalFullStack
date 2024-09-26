78161f7e6ad8c66be7bfaa955c10f44663be2078
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-09-26 14:01:53 -0600

    se hizo la creación del modelo de libro para poder tener una bd que permita agregar libros solo si la cuenta está registrada.

--- backendjango/apps/libros/models.py
+++ backendjango/apps/libros/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from apps.usuarios.models import Registro
 # Create your models here.
+class Libros(models.Model):
+    titulo = models.CharField(max_length=30)
+    usuarioLibro = models.ForeignKey(Registro, on_delete=models.CASCADE)
+    autor = models.CharField(max_length=30)
+    estado = models.CharField(max_length=30)
+    castegoria = models.CharField(max_length=30)
+    ubicacion = models.CharField(max_length=30)
+    
+    def __str__(self) -> str:
+        return self.titulo
