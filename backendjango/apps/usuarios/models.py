e2c270291b2cc1b8adf15c0f07fd54a34dbb2a6c
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-10-01 21:39:11 -0600

    cambios en los estilos y el modal.

--- backendjango/apps/usuarios/models.py
+++ backendjango/apps/usuarios/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 # Creamos la tabla Registro, esta se verá en la base de datos
 class Registro(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     telefono = models.CharField(max_length=20)
     ubicacion = models.CharField(max_length=100)
