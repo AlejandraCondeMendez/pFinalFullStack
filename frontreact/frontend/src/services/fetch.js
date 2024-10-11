c63bf88d6d34b622398565d488a92049a114a14b
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-10-11 10:35:20 -0600

    Arreglo en el fetch de putData.

--- frontreact/frontend/src/services/fetch.js
+++ frontreact/frontend/src/services/fetch.js
@@ -1,4 +1,3 @@
-import { json } from "react-router-dom";
 import { muestraAlerta } from "./alertas";
 
 const api = ('http://127.0.0.1:8000/api/')
@@ -79,7 +78,7 @@ async function deleteData(endpoint, id='') {
 }
 export {deleteData}
 
-const function putData(obj, endpoint){
+async function putData(obj, endpoint) {
     try {
         const response = await fetch(api+endpoint+'/'+obj.id, {
             method: 'PUT',
@@ -91,11 +90,10 @@ const function putData(obj, endpoint){
             body: JSON.stringify(obj)
         });
         const data = await response.json();
-        return data
-        
+        return data   
     } catch (e) {
         console.log(e);
         return null;
-    }
-}
+    }  
+} 
 export default {putData}
